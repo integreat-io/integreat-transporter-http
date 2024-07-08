c32fa981d043c2e7283ca4b06e73329138f2bcbc
Kjell-Morten <post@kjellmorten.no>
2024-07-08 20:24:44 +0200

    Treat query params ending in brackets, as an array

--- src/utils/request.test.ts
+++ src/utils/request.test.ts
@@ -45,3 +45,45 @@ test('should lowercase host when creating action from request', async (t) => {
 
   t.deepEqual(ret, expectedAction)
 })
+
+test('should treat query params ending in brackets as an array', async (t) => {
+  const request = {
+    method: 'GET',
+    url: '/entries?ids[]=ent1&ids[]=ent2&archived=true&pageSize=100',
+    headers: {
+      host: 'localhost',
+      'content-type': 'application/json',
+    },
+    [Symbol.asyncIterator]() {
+      return {
+        next() {
+          return Promise.resolve({ value: undefined, done: true })
+        },
+      }
+    },
+  } as IncomingMessage
+  const expectedAction = {
+    type: 'GET',
+    payload: {
+      method: 'GET',
+      hostname: 'localhost',
+      port: 9030,
+      path: '/entries',
+      queryParams: {
+        ids: ['ent1', 'ent2'],
+        archived: 'true',
+        pageSize: '100',
+      },
+      contentType: 'application/json',
+      headers: {
+        'content-type': 'application/json',
+        host: 'localhost',
+      },
+    },
+    meta: {},
+  }
+
+  const ret = await actionFromRequest(request, 9030)
+
+  t.deepEqual(ret, expectedAction)
+})
